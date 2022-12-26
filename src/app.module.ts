dc8211a3a363ce2e4bedc09f11e379cccea38b6e
Lucas <lucasrafael123araujolima@gmail.com>
2022-12-26 08:21:52 -0400

    feat: :sparkles:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { PrismaService } from './server/prisma.service';
 import { Module } from '@nestjs/common';
-import { AppController } from './server/app.controller';
+
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
+import { MessagingModule } from './infra/messaging/messaging.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [PrismaService],
+  imports: [HttpModule, DatabaseModule, MessagingModule],
 })
 export class AppModule {}
